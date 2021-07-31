1848f7a2c3a0e210d1aaf2d7ce58db5cd3a8205f
Conor Kelly <L00098694@student.lyit.ie>
2021-07-31 17:34:42 +0100

    feat: use random words for song titles

--- services/pexels.js
+++ services/pexels.js
@@ -1,27 +1,44 @@
 // Pexels
 const pexels = require('pexels');
 const pexelsClient = pexels.createClient(process.env.PEXELS_API_KEY);
+const Posts = require('../models/Post');
+
+const axios = require('axios');
 
 // Helper function
-function generateTitle(url) {
-  const title = url.split('https://www.pexels.com/photo/')[1].split('-')[0];
-  return title.charAt(0).toUpperCase() + title.slice(1);
+function toUppercase(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-async function getCuratedPhotos({ count }) {
-  const pexelResponse = await pexelsClient.photos.curated({ per_page: count });
-  const posts = pexelResponse.photos.map((photo) => {
-    return {
-      id: photo.id,
-      title: generateTitle(photo.url),
-      createdAt: new Date().toISOString(),
-      imageSrc: {
-        large: photo.src.large,
-        medium: photo.src.medium,
-        small: photo.src.small,
-      },
-    };
+async function getCuratedPhotos({ count, pageSize }) {
+  let existingPhotoIds = await Posts.find({}).select({ _id: 0, pexelsId: 1 });
+  existingPhotoIds = new Set(existingPhotoIds.map((p) => p.pexelsId));
+
+  const randomWords = await axios.get(
+    `https://random-word-api.herokuapp.com/word?swear=0&number=${count}`
+  );
+  const pexelResponse = await pexelsClient.photos.curated({
+    per_page: Math.max(pageSize, count),
   });
+
+  const posts = [];
+  for (const photo of pexelResponse.photos) {
+    if (posts.length === count) {
+      break;
+    }
+    if (!existingPhotoIds.has(photo.id)) {
+      posts.push({
+        pexelsId: photo.id,
+        title: toUppercase(randomWords.data.pop()),
+        createdAt: new Date().toISOString(),
+        imageSrc: {
+          large: photo.src.large,
+          medium: photo.src.medium,
+          small: photo.src.small,
+        },
+      });
+    }
+  }
 
   return posts;
 }
