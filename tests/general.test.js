5ae2ca599368ba4151887568009a338545ceda93
Conor Kelly <L00098694@student.lyit.ie>
2021-08-17 17:14:23 +0100

    tests: add general test

---
+++ tests/general.test.js
@@ -0,0 +1,39 @@
+// Environment Setup
+process.env.NODE_ENV = 'test';
+process.env.PORT = 9876;
+process.env.JWT_SECRET = '012345678901234567890';
+process.env.JWT_EXPIRY_DAYS = '30d';
+
+jest.mock('../services/pexels');
+jest.mock('../services/audioStorage');
+
+// Make sure (in-memory) database is setup before tests
+beforeAll(async () => {
+  const db = require('../config/db');
+  await db.sync();
+});
+
+// Test dependency
+const request = require('supertest');
+
+// Main application
+const app = require('../app.js');
+
+describe('General', () => {
+  it('should handle invalid JSON req body', async () => {
+    const res = await request(app)
+      .post('/auth/login')
+      .set('Content-Type', 'application/json')
+      .send('{ THIS IS NOT VALID JSON');
+    // Verify statusCode matches the expected value for the test case
+    expect(res.statusCode).toEqual(400);
+    // Verify res.body properties match
+    expect(res.body).toHaveProperty('success');
+    expect(res.body.success).toEqual(false);
+
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toEqual('Invalid JSON body provided.');
+
+    expect(Object.keys(res.body).length).toEqual(2);
+  });
+});
