b0658a477dd2aeffb0f781c92a4ebe237dd29f14
Conor Kelly <conormpkelly@gmail.com>
2021-08-18 13:35:24 +0100

    tests: fix pexels mock

--- services/__mocks__/pexels.js
+++ services/__mocks__/pexels.js
@@ -1,13 +1,16 @@
+// For ensuring unique pexelsIds values
+let count = 0;
+
 function getCuratedPhotos() {
   return [
     {
-      pexelsId: 1,
+      pexelsId: count++,
       title: 'Title1',
       createdAt: new Date().toISOString(),
       imageSrc: 'http://test.com/image1.jpg',
     },
     {
-      pexelsId: 2,
+      pexelsId: count++,
       title: 'Title2',
       createdAt: new Date().toISOString(),
       imageSrc: 'http://test.com/image2.jpg',
