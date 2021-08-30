db2dce7069421da59a92670fb737488f02c8070f
Conor Kelly <conormpkelly@gmail.com>
2021-08-31 00:42:40 +0100

    refactor: clean up server.js

--- server.js
+++ server.js
@@ -3,35 +3,38 @@ const app = require('./app');
 const sequelize = require('./config/db');
 const userService = require('./services/auth');
 
-// Wait for DB to connect before starting API
-
-sequelize
-  .sync()
-  .then(() => {
-    userService.findUserByUsername('admin').then((user) => {
-      if (!user) {
-        console.log('No admin user found...');
-
-        userService
-          .createUser({
-            username: 'admin',
-            password: process.env.ADMIN_USER_APP_PASSWORD,
-            isAdmin: true,
-          })
-          .then(() => {
-            console.log('Created admin user!');
-          });
-      } else {
-        console.log('Existing admin user was found.');
-      }
+/**
+ * Performs setup actions before the API starts listening:
+ * - Connects to database and adds models.
+ * - Adds the admin app user if not present.
+ */
+async function setup() {
+  await sequelize.sync();
+  console.log('Connected to database.');
+  const adminUser = await userService.findUserByUsername('admin');
+  if (!adminUser) {
+    await userService.createUser({
+      username: 'admin',
+      password: process.env.ADMIN_USER_APP_PASSWORD,
+      isAdmin: true,
     });
-  })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log('Listening on port 3000');
+    console.log('Created admin user!');
+  }
+}
+
+/**
+ * Wait for setup tasks to complete and begin listening.
+ */
+async function main() {
+  try {
+    await setup();
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}.`);
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log(err);
     process.exit(1);
-  });
+  }
+}
+
+main();
